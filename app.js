47b16f05e186bd45c39eb6f0bbc8c1701968c7db
kelvin9314 <kelvin9314@gmail.com>
2020-03-12 14:52:47 +0800

    Views using React

--- app.js
+++ app.js
@@ -16,14 +16,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static(path.join(__dirname, 'build')))
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './views/build')))
 
-app.use('/', indexRouter);
+app.get('/api', (req, res) => {
+  res.json('api testing')
+})
 // serve the static html which is build React App
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'))
-// })
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/build', 'index.html'))
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
