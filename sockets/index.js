958a6cb7fcdac365b0c34c845fa9da9301ed98d0
kelvin9314 <kelvin9314@gmail.com>
2020-03-23 11:06:48 +0800

    refactor: socker server side

---
+++ sockets/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const SocketServer = require('socket.io')
+
+const app = express()
+app.io = new SocketServer()
+
+let userCounter = 0
+
+module.exports = io => {
+  io.on('connection', socket => {
+    let isUserAdded = false
+
+    socket.broadcast.emit('hi')
+    console.log('New client connected')
+    socket.on('add user', userName => {
+      if (isUserAdded) return
+      socket.userName = userName
+      isUserAdded = true
+      userCounter += 1
+      console.log(userName)
+
+      socket.broadcast.emit('user joined', {
+        userName: userName,
+        userCounter
+      })
+    })
+
+    socket.on('new message', data => {
+      const { userName, message } = data
+      console.log(userName, message)
+      socket.broadcast.emit('new message', {
+        userName,
+        message
+      })
+    })
+
+    socket.on('disconnect', () => {
+      console.log('Client disconnected')
+    })
+  })
+}
